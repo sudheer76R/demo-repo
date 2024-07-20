9106947666318303b9b2b60552c21d365670e5f8
sudheer76R <106054260+sudheer76R@users.noreply.github.com>
2024-07-20 16:38:00 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+env=${env_name}
+execute=${myboolean}
 echo "Hello!! Artisan-Tek!!"
+echo "Executing in ${env}--Environment"
+if [[ $execute == "true" ]]; then
+echo "Executing the task"
+else
+echo "skipping the task"
+fi
