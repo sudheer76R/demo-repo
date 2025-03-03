143459a630b40f193023e1fb713c66454e9f3e01
sudheer76R <106054260+sudheer76R@users.noreply.github.com>
2025-03-03 23:10:02 +0530

    Update resource_util.sh

--- resource_util.sh
+++ resource_util.sh
@@ -5,16 +5,17 @@ echo "CPU Utilization:"
 mpstat 1 1 | awk 'NR==4 {print "Average CPU Usage: " 100-$NF "%"}'
 
 # Check Memory utilization
-echo ""
+echo "========================"
 echo "Memory Utilization:"
 free -m | awk 'NR==2 {printf "Used Memory: %s MB (%.2f%%)\n", $3, ($3/$2)*100}'
 
 # Check Disk utilization
-echo ""
+echo "========================"
 echo "Disk Utilization:"
 df -h | awk 'NR>1 {printf "%s\t%s\t%s\t%s\n", $1, $2, $5, $6}'
 
 # Check top 5 processes by CPU usage
-echo ""
+echo "========================"
 echo "Top 5 Processes by CPU Usage:"
 ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 6
+echo "========================"
